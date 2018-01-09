5e64ffc0d15fd0ddb96f802c4a4ab7975a191eb8
shaun-cs <sbaker@easynet.co.uk>
2018-01-09 17:07:06 +0100

    removed commented out problematic namespace

--- cc-108/escaped.h
+++ cc-108/escaped.h
@@ -8,29 +8,26 @@
 #pragma once
 #include <string>
 
-//namespace util
-//{
-    //provide escaped textual representation of value
-    //- any double quotes need escaping with \
-    //- wrap in double quotes if has any spaces
-    template <typename T>
-    std::string escaped_text(T t)
-    {
-        using namespace std;
+//provide escaped textual representation of value
+//- any double quotes need escaping with \
+//- wrap in double quotes if has any spaces
+template <typename T>
+std::string escaped_text(T t)
+{
+    using namespace std;
 
-        auto ret = to_string(t);
-        for(std::size_t idx = 0;
-            (idx = ret.find(idx, '"')) != std::string::npos;
-                idx += 2)
-        {
-            ret.insert(idx, "\\", 1);
-        }
-        if(ret.find(' ') != std::string::npos)
-        {
-            ret = '"' + ret + '"';
-        }
-        return ret;
+    auto ret = to_string(t);
+    for(std::size_t idx = 0;
+        (idx = ret.find(idx, '"')) != std::string::npos;
+            idx += 2)
+    {
+        ret.insert(idx, "\\", 1);
     }
-//}
+    if(ret.find(' ') != std::string::npos)
+    {
+        ret = '"' + ret + '"';
+    }
+    return ret;
+}
 
 #endif //CODE_CRITIQUE_ESCAPED_H
